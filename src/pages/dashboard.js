bd9ce9cf01b1c5ee488a2e5a7be400df9378c6d8
mhcrocky <larastar721@gmail.com>
2022-12-08 16:56:43 +0400

    intergrate to sakai

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -14,15 +14,6 @@ const Dashboard = () => {
                 <title>Laravel - Dashboard</title>
             </Head>
 
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 bg-white border-b border-gray-200">
-                            You're logged in!
-                        </div>
-                    </div>
-                </div>
-            </div>
         </AppLayout>
     )
 }
